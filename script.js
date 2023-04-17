688e48e449702a4280d1c1bc8a1d8e55bf537c2e
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-04-17 16:26:23 +0200

    edit script.js

--- script.js
+++ script.js
@@ -27,59 +27,54 @@ let computerChoice = getComputerChoice(choices);
 
 // this function takes the players choice and the computers choice and checks who has won the round
 function playRound() {
-    if (playerChoice === computerChoice) {
-      let computerResultID = document.getElementById('result');
-      computerResultID.innerText = 'That is a tie!';
-      computerResultID.style.color = 'white';
-    }
+  if (playerChoice === computerChoice) {
+    let computerResultID = document.getElementById('result');
+    computerResultID.innerText = 'That is a tie!';
+    computerResultID.style.color = 'white';
+  }
 
-    else if (playerChoice === "ROCK" && computerChoice === "PAPER") {
-        computerScore++;
-        updateScoreBoard(userScore, computerScore);
-        updateComputerResult();
-    }
+  else if (playerChoice === "ROCK" && computerChoice === "PAPER") {
+      computerScore++;
+      updateScoreBoard(userScore, computerScore);
+      updateComputerResult();
+  }
 
-    else if (playerChoice === "PAPER" && computerChoice === "ROCK") {
-        userScore++;
-        updateScoreBoard(userScore, computerScore);
-        updatePlayerResult();
-    }
+  else if (playerChoice === "PAPER" && computerChoice === "ROCK") {
+      userScore++;
+      updateScoreBoard(userScore, computerScore);
+      updatePlayerResult();
+  }
 
-    else if (playerChoice === "ROCK" && computerChoice === "SCISSORS") {
-        userScore++;
-        updateScoreBoard(userScore, computerScore);
-        updatePlayerResult();
-    }
+  else if (playerChoice === "ROCK" && computerChoice === "SCISSORS") {
+      userScore++;
+      updateScoreBoard(userScore, computerScore);
+      updatePlayerResult();
+  }
 
-    else if (playerChoice === "SCISSORS" && computerChoice === "ROCK") {
-        computerScore++;
-        updateScoreBoard(userScore, computerScore);
-        updateComputerResult();
-    }
+  else if (playerChoice === "SCISSORS" && computerChoice === "ROCK") {
+      computerScore++;
+      updateScoreBoard(userScore, computerScore);
+      updateComputerResult();
+  }
 
-    else if (playerChoice === "PAPER" && computerChoice === "SCISSORS") {
-        computerScore++;
-        updateScoreBoard(userScore, computerScore);
-        updateComputerResult();
-    }
+  else if (playerChoice === "PAPER" && computerChoice === "SCISSORS") {
+      computerScore++;
+      updateScoreBoard(userScore, computerScore);
+      updateComputerResult();
+  }
 
-    else if (playerChoice === "SCISSORS" && computerChoice === "PAPER") {
-        userScore++;
-        updateScoreBoard(userScore, computerScore);
-        updatePlayerResult();
-    };
+  else if (playerChoice === "SCISSORS" && computerChoice === "PAPER") {
+      userScore++;
+      updateScoreBoard(userScore, computerScore);
+      updatePlayerResult();
+  };
 };
 
 // for each button in the "buttons" nodelist, add an event listener for when the button is clicked
 // the ID of the button should be saved in the userChoice variable initialised above and the variable
 // is the passed to the getUserChoice function
 buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    userChoice = button.id;
-    playerChoice = getUserChoice(userChoice);
-    computerChoice = getComputerChoice(choices);
-    playRound();
-  });
+  button.addEventListener('click', handleClick);
 });
 
 // this function simply returns the value of the ID of the clicked button
@@ -111,3 +106,45 @@ function updateComputerResult() {
   computerResultID.innerText = "You lose " + computerChoice + " beats " + playerChoice + "!";
   computerResultID.style.color = '#f9525a';
 };
+
+function checkPoints() {
+  if (userScore >= 5) {
+    buttons.forEach((button) => {
+      button.removeEventListener('click', handleClick);
+    })
+    // let refreshPage = document.getElementById("header-text")
+    // refreshPage.innerText = "Refresh the page to play again!"
+    // refreshPage.style.color = "white"
+    let gameEnd = document.getElementById('result');
+    gameEnd.innerText = "Congratulations You win!!";
+    gameEnd.style.color = '#60f952';
+    refreshPage();
+  }
+
+  else if (computerScore >= 5) {
+    buttons.forEach((button) => {
+      button.removeEventListener('click', handleClick);
+    });
+    let gameEnd = document.getElementById('result');
+    gameEnd.innerText = "Unfortunately Computer win!!";
+    gameEnd.style.color = '#f9525a';
+    refreshPage();
+  };
+};
+
+function handleClick() {
+  userChoice = this.id;
+  playerChoice = getUserChoice(userChoice);
+  computerChoice = getComputerChoice(choices);
+  playRound();
+  checkPoints();
+  const removeHeader = document.getElementById('header-text')
+  container.removeChild(removeHeader)
+};
+
+function refreshPage() {
+  const refreshPage = document.createElement("div")
+  refreshPage.textContent = "Refresh the page to play again!"
+  refreshPage.style.color = "white"
+  container.appendChild(refreshPage)
+}
